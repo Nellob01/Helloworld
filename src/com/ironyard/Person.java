10a5426472e24b797d46f2e8d4f89fae69df2308
Noel <noel@thes-mbp.apollosrc.root>
2016-11-14 11:28:03 -0600

    Helloworld

---
+++ src/com/ironyard/Person.java
@@ -0,0 +1,10 @@
+package com.ironyard;
+
+/**
+ * Created by noel on 11/14/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+}
---
+++ src/com/ironyard/main.java
@@ -0,0 +1,14 @@
+package com.ironyard;
+
+/**
+ * Created by noel on 11/14/16.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        Person p = new Person ();
+        p. name = "Alice";
+        p. age = 30;
+        p.isAlive = true;
+    }
+}
